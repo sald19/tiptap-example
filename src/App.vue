c53048b6f1aa5740e0731b6b883698482d112acd
Carlos Saldaña <carl.sald19@gmail.com>
2021-09-17 17:06:38 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <tiptap-editor v-model="content" class="editor" />
+  </div>
+</template>
+
+<script>
+import TiptapEditor from "./components/TiptapEditor";
+
+export default {
+  name: "App",
+  components: {
+    TiptapEditor,
+  },
+  data() {
+    return { content: "" };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.ProseMirror * {
+  margin: 0;
+  margin-bottom: 0.75em;
+}
+
+.editor {
+  margin: 0;
+  border: 1px solid #dfdfdf;
+}
+</style>
---
+++ src/components/TiptapEditor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent } from "@tiptap/vue-2";
+import StarterKit from "@tiptap/starter-kit";
+
+export default {
+  name: "TiptapEditor",
+
+  components: {
+    EditorContent,
+  },
+
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+
+  data() {
+    return { editor: null };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: this.value,
+      extensions: [StarterKit],
+    });
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
+  },
+};
+</script>
