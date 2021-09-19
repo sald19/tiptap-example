8df500fcd5380d6dfdeb2299fb9c85b2a69dd967
sald19 <carl.sald19@gmail.com>
2021-09-18 18:29:01 -0600

    config tiptap extensions

--- src/components/TiptapEditor.vue
+++ src/components/TiptapEditor.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <editor-content :editor="editor" />
+  <div class="m-2 border border-gray-300 rounded" v-if="editor">
+    <menu-bar :editor="editor" />
+
+    <editor-content :editor="editor" class="h-48 overflow-y-auto" />
   </div>
 </template>
 
@@ -8,11 +10,19 @@
 import { Editor, EditorContent } from "@tiptap/vue-2";
 import StarterKit from "@tiptap/starter-kit";
 
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import Highlight from "@tiptap/extension-highlight";
+import CharacterCount from "@tiptap/extension-character-count";
+
+import MenuBar from "./MenuBar.vue";
+
 export default {
   name: "TiptapEditor",
 
   components: {
     EditorContent,
+    MenuBar,
   },
 
   props: {
@@ -29,7 +39,21 @@ export default {
   mounted() {
     this.editor = new Editor({
       content: this.value,
-      extensions: [StarterKit],
+      editorProps: {
+        attributes: {
+          class:
+            "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+        },
+      },
+      extensions: [
+        CharacterCount.configure({
+          limit: 10000,
+        }),
+        StarterKit,
+        Highlight,
+        TaskList,
+        TaskItem,
+      ],
     });
   },
 
@@ -38,3 +62,9 @@ export default {
   },
 };
 </script>
+
+<style>
+.prose {
+  @apply min-h-full outline-none p-2;
+}
+</style>
