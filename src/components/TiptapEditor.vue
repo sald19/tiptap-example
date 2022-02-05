2754416895182ac572966b30c69ce8445ff8c947
sald19 <carl.sald19@gmail.com>
2022-02-05 13:36:32 -0600

    collaborative editor

--- src/components/TiptapEditor.vue
+++ src/components/TiptapEditor.vue
@@ -8,12 +8,15 @@
 
 <script>
 import { Editor, EditorContent } from "@tiptap/vue-2";
-import StarterKit from "@tiptap/starter-kit";
-
-import TaskList from "@tiptap/extension-task-list";
-import TaskItem from "@tiptap/extension-task-item";
-import Highlight from "@tiptap/extension-highlight";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
 import CharacterCount from "@tiptap/extension-character-count";
+import Collaboration from "@tiptap/extension-collaboration";
+import Highlight from "@tiptap/extension-highlight";
+import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 
 import MenuBar from "./MenuBar.vue";
 
@@ -33,10 +36,17 @@ export default {
   },
 
   data() {
-    return { editor: null };
+    return { editor: null, provider: null };
   },
 
   mounted() {
+    const ydoc = new Y.Doc();
+
+    this.provider = new WebrtcProvider(
+      "tiptap-collaboration-extension-test",
+      ydoc
+    );
+
     this.editor = new Editor({
       content: this.value,
       editorProps: {
@@ -49,16 +59,29 @@ export default {
         CharacterCount.configure({
           limit: 10000,
         }),
-        StarterKit,
+        Collaboration.configure({
+          document: ydoc,
+        }),
+        CollaborationCursor.configure({
+          provider: this.provider,
+          user: {
+            name: "@sald19",
+            color: "#f783ac",
+          },
+        }),
         Highlight,
-        TaskList,
+        StarterKit.configure({
+          history: false,
+        }),
         TaskItem,
+        TaskList,
       ],
     });
   },
 
-  beforeDestroy() {
+  beforeUnmount() {
     this.editor.destroy();
+    this.provider.destroy();
   },
 };
 </script>
@@ -66,5 +89,32 @@ export default {
 <style>
 .prose {
   @apply min-h-full outline-none p-2;
+}
+
+/* Give a remote user a caret */
+.prose .collaboration-cursor__caret {
+  position: relative;
+  margin-left: -1px;
+  margin-right: -1px;
+  border-left: 1px solid #0d0d0d;
+  border-right: 1px solid #0d0d0d;
+  word-break: normal;
+  pointer-events: none;
+}
+
+/* Render the username above the caret */
+.prose .collaboration-cursor__label {
+  position: absolute;
+  top: -1.4em;
+  left: -1px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  user-select: none;
+  color: #0d0d0d;
+  padding: 0.1rem 0.3rem;
+  border-radius: 3px 3px 3px 0;
+  white-space: nowrap;
 }
 </style>
